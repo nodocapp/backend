a1b54ed5d5f60c0622aad30f1fef63f82ffc14ee
Shqrp <andrea.giurgo@gmail.com>
2022-09-12 19:49:54 +0200

    feat: add auth middleware

---
+++ src/util/middleware.ts
@@ -0,0 +1,14 @@
+import jwt from "koa-jwt";
+import { koaJwtSecret } from "jwks-rsa";
+
+export const auth = jwt({
+    secret: koaJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${process.env.AUTH_ISSUER}/.well-known/jwks.json`
+    }), 
+    audience: process.env.AUTH_AUDIENCE,
+    issuer: process.env.AUTH_ISSUER,
+    algorithms: ["RS256"]
+});
