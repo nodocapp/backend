629e0553b43ff0db3157d1acae6be825144cb7ea
Shqrp <andrea.giurgo@gmail.com>
2022-09-14 15:26:51 +0200

    feat(user): user registry endpoints

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,4 +1,5 @@
 import Router from "@koa/router";
+import registry from "./registry.js";
 import { subToId } from "../../util/utilities.js";
 import { auth } from "../../util/middleware.js";
 import type { AppContext, AppState } from "../../util/typedefs.js";
@@ -8,11 +9,13 @@ const router = new Router<AppState, AppContext>({ prefix: "/user" });
 router.get("getUserData", "/me", auth, async ctx => {
     try {
         ctx.status = 200;
-        ctx.body = await ctx.prisma.user.findUnique({ where: { id: subToId(ctx.state.user.sub)}});
+        ctx.body = await ctx.prisma.user.findUnique({ where: { id: subToId(ctx.state.user.sub) } });
     } catch (err) {
         ctx.status = 500;
         ctx.body = err;
     }    
 });
+
+router.use(registry.routes(), registry.allowedMethods());
 
 export default router;
