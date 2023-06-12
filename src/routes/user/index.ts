ccac2e5c17c3d6c71a3db12c5564fbfeb9994fbd
Andrea Giurgola <Andrea.giurgo@gmail.com>
2023-06-12 23:04:55 +0200

    fix(user): add user not found handlers and remove unnecessary error handler

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -7,15 +7,10 @@ import type { AppContext, AppState } from "types";
 const router = new Router<AppState, AppContext>({ prefix: "/user" });
 
 router.get("getUserData", "/me", auth, async ctx => {
-    try {
-        ctx.status = 200;
-        ctx.body = await ctx.prisma.user.findUnique({
-            where: { id: subToId(ctx.state.user.sub) },
-        });
-    } catch (err) {
-        ctx.status = 500;
-        ctx.body = err;
-    }
+    ctx.status = 200;
+    ctx.body = await ctx.prisma.user.findUnique({
+        where: { id: subToId(ctx.state.user.sub) },
+    });
 });
 
 router.use(registry.routes(), registry.allowedMethods());
--- src/routes/user/registry.ts
+++ src/routes/user/registry.ts
@@ -7,19 +7,31 @@ import { subToId } from "../../util/utilities.js";
 
 const router = new Router<AppState, AppContext>({ prefix: "/registry" });
 
-router.get("getRegistry", "/", auth, async ctx => {
+router.get("getRegistry", "/", auth, async (ctx, next) => {
     const user = await ctx.prisma.user.findUnique({
         where: { id: subToId(ctx.state.user.sub) },
     });
+
+    if (!user) {
+        ctx.status = 404;
+        ctx.body = "The specified user was not found.";
+        return await next();
+    }
+
     ctx.status = 200;
-    ctx.body = user?.registry;
+    ctx.body = user.registry;
 });
 
 router.post("setRegistryField", "/set", auth, koaBody(), async (ctx, next) => {
     const user = await ctx.prisma.user.findUnique({
         where: { id: subToId(ctx.state.user.sub) },
     });
-    const registry = user?.registry as Prisma.JsonObject;
+
+    if (!user) {
+        ctx.status = 404;
+        ctx.body = "The specified user was not found.";
+        return await next();
+    }
 
     if (Object.values(ctx.request.body).length === 0) {
         ctx.status = 400;
@@ -27,30 +39,35 @@ router.post("setRegistryField", "/set", auth, koaBody(), async (ctx, next) => {
         return await next();
     }
 
+    const registry = user.registry as Prisma.JsonObject;
+
     Object.keys(ctx.request.body).forEach(
         f => (registry[f] = ctx.request.body[f])
     );
-    const updateClause = Prisma.validator<Prisma.UserUpdateInput>()({
-        registry: registry,
+
+    await ctx.prisma.user.update({
+        where: { id: subToId(ctx.state.user.sub) },
+        data: { registry: registry },
     });
 
-    await ctx.prisma.$transaction([
-        ctx.prisma.user.update({
-            where: { id: subToId(ctx.state.user.sub) },
-            data: updateClause,
-        }),
-    ]);
     ctx.status = 200;
     ctx.body = `The fields ${Object.keys(ctx.request.body).join(
         ", "
     )} were updated.`;
 });
 
-router.get("getRegistryField", "/:field", auth, async ctx => {
+router.get("getRegistryField", "/:field", auth, async (ctx, next) => {
     const user = await ctx.prisma.user.findUnique({
         where: { id: subToId(ctx.state.user.sub) },
     });
-    const field = (user?.registry as Prisma.JsonObject)[ctx.params.field];
+
+    if (!user) {
+        ctx.status = 404;
+        ctx.body = "The specified user was not found.";
+        return await next();
+    }
+
+    const field = (user.registry as Prisma.JsonObject)[ctx.params.field];
     if (field) {
         ctx.status = 200;
         ctx.body = field;
